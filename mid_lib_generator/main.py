7a774bb2d78116b38da7e556582117264555292b
nrakshit <107974817+nrakshit@users.noreply.github.com>
2022-06-22 23:04:41 +0530

    mad lab complete

---
+++ mid_lib_generator/main.py
@@ -0,0 +1,49 @@
+import json
+import os
+
+class MadLibs:
+    path = "./templates"
+    def __init__(self, word_descriptions, template):
+        self.template = template
+        self.word_descriptions = word_descriptions
+        self.user_input = []
+        self.story = None
+
+    @classmethod
+    def from_json(cls,name, path = None):
+        if not path:
+            path = cls.path
+        fpath = os.path.join(path, name).replace("\\", "/")
+        with open(fpath, "r") as f:
+            data = json.load(f)
+        mat_libs = cls(**data)
+        return mat_libs
+
+    def get_words_from_user(self):
+        print('Please provide the following words: ')
+        for desc in self.word_descriptions:
+            ui = input(desc + " ")
+            self.user_input.append(ui)
+        return self.user_input
+
+    def build_story(self):
+        self.story = self.template.format(*self.user_input)
+        return self.story
+
+    def show_story(self):
+        print(self.story)
+
+def select_template():
+    print("Select a Mad Lab template from the list: ")
+    templates = os.listdir(MadLibs.path)
+    template = input(str(templates) + " ")
+    return template
+
+temp_name = select_template()
+#temp_name = "day_at_the_zoo.json"
+mad_lib = MadLibs.from_json(temp_name)
+words = mad_lib.get_words_from_user()
+story = mad_lib.build_story()
+mad_lib.show_story()
+
+
